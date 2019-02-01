c15c9a045faa89d6d4c5d29e837538354e7f3ea6
Joshua <joshuatundag@gmail.com>
2019-02-02 00:40:52 +0800

    test

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -13,11 +13,10 @@ class UserRepository extends Repository implements TableContract{
 	public function forTable(\Illuminate\Http\Request $request){
 		$query = $this->model->whereIs($request->role)
 					->with('department');
-
 		if($request->department_id){
 			$query = $query->where('department_id', $request->department_id);
 		}
-		
+		dd($query->get());
 		return [
 			'result' => $query->get(),
 		];
