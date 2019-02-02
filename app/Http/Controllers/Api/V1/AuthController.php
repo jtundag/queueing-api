626b549565aea6d32202cd4b5b99f0d04867b7e0
Joshua <joshuatundag@gmail.com>
2019-02-02 20:32:56 +0800

    test fix

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -62,6 +62,8 @@ class AuthController extends Controller
         
         try {
             $user = \JWTAuth::parseToken()->authenticate();
+            dd($user);
+            die;
             $user['roles'] = $user->roles;
             $user['department'] = $user->department;
         } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
