3c83a2b00738e14c9b44d5f049b9bb15c40b8239
Joshua <joshuatundag@gmail.com>
2019-02-03 22:31:46 +0800

    Update AuthController.php

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -110,7 +110,10 @@ class AuthController extends Controller
     * @return \Illuminate\Http\JsonResponse
     */
     protected function respondWithToken($token){
-        $user = $this->guard()->user()->with('roles', 'department');
+        //$user = $this->guard()->user()->with('roles', 'department');
+        $user = $this->guard()->user();
+        $user['roles'] = $user->roles;
+        $user['department'] = $user->department;
         return response()->json([
             'access_token' => $token,
             'user' => $user,
